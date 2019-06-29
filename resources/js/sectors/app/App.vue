7784234ef8854095e01879e6b91c312cc986b4c1
judicium <judicium@chinookWD.ca>
2019-06-29 01:53:36 -0600

    [CHORE]: set up front end

---
+++ resources/js/sectors/app/App.vue
@@ -0,0 +1,18 @@
+<template>
+<article class="app">
+    <ExampleComponent />
+</article>
+</template>
+
+<script>
+import ExampleComponent from './_components/ExampleComponent.vue';
+
+export default {
+    components: {
+        ExampleComponent: ExampleComponent
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ resources/js/sectors/app/_components/ExampleComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1> Im an example component </h1>
+</template>
+
+<script>
+</script>
