d4dab29578c80dc2fb1460dd4fb3a85e47708ec7
judicium <judicium@chinookWD.ca>
2019-06-30 14:50:04 -0600

    [FEAT]: added edge case views

---
+++ resources/ts/sectors/app/_views/LoadingView/LoadingView.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    h1 Loading...
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LoadingView extends Vue {}
+</script>
+
---
+++ resources/ts/sectors/app/_views/TimeoutView/TimeoutView.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    h1 The page has timed out
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TimeoutView extends Vue {}
+</script>
+
