87e2a0e838fd296d72dae9564f516af1d3f1f84e
judicium <judicium@chinookWD.ca>
2019-07-02 03:14:04 -0600

    [FEAT]: added vee-validate

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,6 +1,7 @@
 import App from "./sectors/app/App.vue";
 import Vue from "vue";
 import VueApollo from "vue-apollo";
+import VeeValidate from "vee-validate";
 import Auth from "./common/plugins/auth.plugin";
 import { apolloClient } from "./common/config/apollo.config";
 import { router } from "./common/config/router.config";
@@ -8,6 +9,7 @@ import { router } from "./common/config/router.config";
 import './bootstrap';
 
 Vue.use(VueApollo);
+Vue.use(VeeValidate);
 Vue.use(Auth);
 
 var VueApp: any = Vue;
