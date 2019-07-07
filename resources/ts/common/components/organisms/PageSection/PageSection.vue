ecc363e42875ef29a5832128fc33761d8708f19f
judicium <judicium@chinookWD.ca>
2019-07-07 17:35:18 -0600

    [FEAT]: Add page sections component

---
+++ resources/ts/common/components/organisms/PageSection/PageSection.vue
@@ -0,0 +1,79 @@
+<template lang='pug'>
+section( :class="pageSectionClasses" )
+    grid
+        fade-animation
+            grid-row( :class="[flip ? 'flip' : '']" )
+                grid-item.image
+                    cur-image( :src="image", :alt="image" )
+                grid-item.text
+                    h3 {{ title }}
+                    slot( name="text" )
+                    br
+                    br
+                    cur-button( variant="primary", v-if="link", tag="router-link", :target="link" )
+                        slot( name="button" )
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import FadeAnimation from "../../../animations/FadeAnimation/FadeAnimation.vue";
+
+@Component({
+    components: {
+        FadeAnimation: FadeAnimation,
+    }
+})
+export default class PageSection extends Vue {
+    @Prop({ required: true }) image!: string;
+    @Prop({ default: '' }) background!: string;
+    @Prop({ default: false }) flip!: boolean;
+    @Prop({ default: '' }) link!: string;
+    @Prop({ default: '' }) title!: string;
+
+    get pageSectionClasses() {
+        const classes: string[] = ['pageSection'];
+
+        classes.push(`${this.background}`);
+
+        return classes;
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '~styles/app';
+
+.pageSection {
+    padding: space(52) 0;
+
+    @include media(tabletPortrait) {
+        padding: space(84) space(36);
+    }
+}
+
+.flip {
+    flex-direction: row-reverse;
+}
+
+.image {
+    min-height: 256px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50%;
+    margin-bottom: space(36);
+
+    @include media(tabletPortrait) {
+        min-height: 512px;
+        margin-bottom: 0;
+    }
+}
+
+/**
+ * Backgrounds
+ */
+
+.alternative {
+    background: color("accent");
+    color: color("text", "inverse");
+}
+</style>
