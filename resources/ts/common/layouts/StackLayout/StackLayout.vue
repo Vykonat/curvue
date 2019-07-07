cdf26786ff3478e22492f958e0979d66f967bab4
judicium <judicium@chinookWD.ca>
2019-07-07 17:34:51 -0600

    [FEAT]: Add stack layout for full width layouts

---
+++ resources/ts/common/layouts/StackLayout/StackLayout.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.StackLayout
+    navigation
+    .contentBelowNav
+        breadcrumb( :items="$route.meta.breadcrumbs" )
+        slot
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Navigation from "../../../sectors/app/_components/Navigation/Navigation.vue";
+
+@Component({
+    components: {
+        Navigation: Navigation
+    }
+})
+export default class DefaultLayout extends Vue {}
+</script>
+
