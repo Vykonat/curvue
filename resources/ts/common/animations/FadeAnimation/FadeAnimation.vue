7904f2a39eb2d2331c09236d01d6b8ea237868bf
judicium <judicium@chinookWD.ca>
2019-07-07 17:01:33 -0600

    [FEAT]: Add fade animation

---
+++ resources/ts/common/animations/FadeAnimation/FadeAnimation.vue
@@ -0,0 +1,40 @@
+<template>
+    <transition
+        enter-class="enter"
+        enter-active-class="enterActive"
+        enter-to-class="enterTo"
+        leave-class="leave"
+        leave-active-class="$leaveActive"
+        leave-to-class="leaveTo"
+    >
+        <slot />
+    </transition>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class FadeAnimation extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/app';
+
+.enterActive,
+.leaveActive,
+.enterTo,
+.leaveTo {
+    transition: opacity 1000ms ease-in-out;
+}
+
+.enterTo,
+.leave {
+    opacity: 1;
+}
+
+.enter,
+.leaveTo {
+    opacity: 0;
+}
+</style>
