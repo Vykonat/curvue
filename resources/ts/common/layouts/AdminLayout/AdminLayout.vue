2ecf1af17a6d1583706b8786e201c8b17e3853f6
judicium <judicium@chinookWD.ca>
2019-07-08 07:18:22 -0600

    [FEAT]: Add administrator layout

---
+++ resources/ts/common/layouts/AdminLayout/AdminLayout.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.AdminLayout
+    admin-navigation
+    .contentBelowNav
+        grid
+            slot
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import AdminNavigation from "../../../sectors/app/_components/AdminNavigation/AdminNavigation.vue";
+
+@Component({
+    components: {
+        AdminNavigation: AdminNavigation
+    }
+})
+export default class AdminLayout extends Vue {}
+</script>
+
---
+++ resources/ts/sectors/app/_components/AdminNavigation/AdminNavigation.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+article.navigation
+    navbar
+        template( v-slot:middle )
+            router-link.brand( :to="{ name: 'app.home' }" ) {{ appName }}
+        template( v-if="$auth.check()", v-slot:right )
+            cur-button( :is-ghost="true", tag="router-link", :target="{ name: 'app.home' }", variant="accent" ) Return to app
+
+    nav-drawer
+        nav-drawer-group( title="Navigation" )
+            nav-drawer-group-item( icon="fas fa-users" :to="{ name: 'admin.users' }" ) Users
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { APP_NAME } from "../../../../common/config/app.config";
+
+@Component
+export default class Navigation extends Vue {
+
+    get appName() {
+        return APP_NAME;
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '~styles/app';
+
+.brand {
+    font-size: fontSize(h3);
+    font-weight: bold;
+    color: color("text");
+
+    &:hover {
+        color: color("text");
+    }
+}
+</style>
