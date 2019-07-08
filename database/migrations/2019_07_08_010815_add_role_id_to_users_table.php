46ac2391a1a7785635bdbb855f6d20479b89fb5a
judicium <judicium@chinookWD.ca>
2019-07-07 19:10:13 -0600

    [FEAT]: Add role id to users table

---
+++ database/migrations/2019_07_08_010815_add_role_id_to_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoleIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $driver = Schema::connection($this->getConnection())->getConnection()->getDriverName();
+
+        Schema::table('users', function (Blueprint $table) use ($driver) {
+            if($driver === 'sqlite') {
+                $table->unsignedSmallInteger('role_id')->default(0)->after('id');
+            } else {
+                $table->unsignedSmallInteger('role_id')->after('id');
+            }
+            
+            $table->foreign('role_id')->references('id')->on('user_roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+        });
+    }
+}
