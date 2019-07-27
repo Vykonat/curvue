9b0fc4b6a24c030d16201e10748bd772651c3553
judicium <_judicium@tuta.io>
2019-07-26 18:21:46 -0600

    [TEST]: Add data table tests

--- resources/ts/common/components/organisms/DataTable/DataTable.spec.ts
+++ resources/ts/common/components/organisms/DataTable/DataTable.spec.ts
@@ -33,9 +33,78 @@ describe('Data Table Organism Component', () => {
         data,
         header,
         showSearch: false
-      }
+      },
+      stubs: [
+        'lvql-select',
+        'grid-item',
+        'grid-row',
+        'pagination',
+        'panel',
+        'panel-body',
+        'data-table-header',
+        'data-table-search'
+      ]
     });
 
     expect(wrapper.findAll(`.dataTableSearch`)).toHaveLength(0);
+  });
+
+  test('Filters the data by search term', () => {
+    const wrapper = shallowMount(DataTable, {
+      propsData: {
+        data,
+        header
+      },
+      stubs: [
+        'lvql-select',
+        'grid-item',
+        'grid-row',
+        'pagination',
+        'panel',
+        'panel-body',
+        'data-table-header',
+        'data-table-search'
+      ]
+    }) as any;
+
+    wrapper.vm.searchTerm = 'julia';
+    expect(wrapper.findAll('.dataTableRow')).toHaveLength(4);
+
+    wrapper.vm.searchTerm = 'z';
+    expect(wrapper.findAll('.dataTableNoResults')).toHaveLength(1);
+    expect(wrapper.findAll('.dataTableRow')).toHaveLength(0);
+  });
+
+  test('Sorts data by sort key', () => {
+    const wrapper = shallowMount(DataTable, {
+      propsData: {
+        data,
+        header
+      },
+      stubs: [
+        'lvql-select',
+        'grid-item',
+        'grid-row',
+        'pagination',
+        'panel',
+        'panel-body',
+        'data-table-header',
+        'data-table-search'
+      ]
+    }) as any;
+
+    wrapper.setProps({ sortKey: 'firstname' });
+
+    expect(wrapper.vm.sortedData[0].firstname).toBe('Julia');
+    expect(wrapper.vm.sortedData[1].firstname).toBe('Julia');
+    expect(wrapper.vm.sortedData[2].firstname).toBe('Julia');
+    expect(wrapper.vm.sortedData[3].firstname).toBe('Julia');
+
+    wrapper.setProps({ sortDirection: 'desc' });
+
+    expect(wrapper.vm.sortedData[0].firstname).toBe('Toni');
+    expect(wrapper.vm.sortedData[1].firstname).toBe('Toni');
+    expect(wrapper.vm.sortedData[2].firstname).toBe('Toni');
+    expect(wrapper.vm.sortedData[3].firstname).toBe('Toni');
   });
 });
