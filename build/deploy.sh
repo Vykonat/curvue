b2f514e9bc68a7eb1d6348d5e7db8481ef5aa2e1
judicium <_judicium@tuta.io>
2019-07-29 15:40:30 -0600

    [FEAT]: Add auto deploy scripts

---
+++ build/deploy.sh
@@ -0,0 +1,33 @@
+# Save latest Git commit message
+MESSAGE=$(git log -1 --pretty=%B)
+
+if git ls-remote $REMOTE_REPOSITORY | grep -sw "deploy" 2>&1>/dev/null; then
+    git checkout . && git checkout deploy;
+else
+    git checkout -b deploy;
+fi
+
+# Remove all files except .git, node_modules and vendor folder
+find . -path ./.git -prune -o \( \! -path ./dist \) -prune -o \( \! -path ./vendor \) -exec rm -rf {} \; 2> /dev/null
+
+# Add master branch files
+git archive master | tar x -C .
+
+# Update composer
+composer update
+
+# Compile front-end
+yarn --pure-lockfile
+yarn production
+
+# Add ignored compiled files
+git add -f public/**/*.css
+git add -f public/**/*.js
+
+# Push to the deploy branch
+git add .
+git commit -am "[${CI_COMMIT_SHA:0:8}]: $MESSAGE"
+git push origin deploy
+
+# Deploy the deploy branch to production
+dep deploy
