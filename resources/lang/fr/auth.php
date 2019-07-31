fc44f0681103e4f0c8a73369cbc87449fc7f238e
judicium <_judicium@tuta.io>
2019-07-30 18:36:58 -0600

    add french translation strings

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login' => 'S\'identifier',
+    'login_fail' => 'Vous avez entré un nom d\'utilisateur ou un mot de passe invalide',
+    'login_success' => 'Vous êtes maintenant connecté',
+    'register' => 'Registre',
+    'register_fail' => 'Il y avait un problème avec l\'enregistrement, l\'e-mail peut déjà être pris',
+    'register_success' => 'Vous vous êtes inscrit avec succès et vous pouvez maintenant vous connecter',
+    'logout' => 'Connectez - Out',
+    'logged_out' => 'vous avez été déconnecté',
+    'reset' => 'réinitialiser le mot de passe',
+    'reset_fail' => 'Il y avait un problème avec la réinitialisation de votre mot de passe, votre jeton peut être invalide',
+    'forgot' => 'Mot de passe oublié',
+    'forgot_submitted' => 'Nous vous avons envoyé un courrier électronique avec les instructions pour réinitialiser votre mot de passe',
+    'remember' => 'Souviens-toi de moi'
+];
---
+++ resources/lang/fr/core.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'slogan' => 'Un PSC Laravel Vue et GraphQL PWA qui met l’accent sur les performances et les meilleures pratiques.',
+    'cta' => 'Code source',
+    'loading' => 'Chargement...',
+    'ok' => 'D\'accord',
+    'cancel' => 'Annuler',
+    'confirm' => 'Confirmer',
+    'new' => 'Nouveau :resource',
+];
