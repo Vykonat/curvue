cc656923eeaac735ad338ef50b350dab42a3d253
judicium <_judicium@tuta.io>
2019-11-29 14:26:26 -0600

    Add missing french strings

---
+++ resources/lang/fr/blogPosts.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'titre',
+    'title_placeholder' => 'Entrez un titre pour le blog',
+    'description' => 'la description',
+    'description_placeholder' => 'Entrez une description pour le blog',
+    'content' => 'contenu',
+    'content_placeholder' => 'Entrez du contenu pour le blog (min: 30)'
+];
---
+++ resources/lang/fr/comments.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'content' => 'contenu',
+    'content_placeholder' => 'Entrez le corps pour votre commentaire',
+    'no_comments' => 'Soyez le premier à commenter cette histoire',
+    'count_single' => 'Commentez cette histoire',
+    'count_plural' => 'Commentaires sur cette histoire'
+];
