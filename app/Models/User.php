0d91cdd4a3acf70eef1fde9771b125099bd3da8f
judicium <_judicium@tuta.io>
2019-12-06 17:33:42 -0600

    refactor user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,22 +5,22 @@ namespace App\Models;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Http\Traits\DateAttributeTransformations;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+    use DateAttributeTransformations;
 
     /**
      * The "booting" method of the model.
-     *
-     * @return void
      */
     protected static function boot()
     {
         parent::boot();
 
-        // auto-sets values on creation and update
         static::saving(function ($user) {
             if(! isset($user->role_id)) {
                 $user->role_id = 2;
@@ -30,8 +30,6 @@ class User extends Authenticatable implements JWTSubject
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array
      */
     protected $fillable = [
         'name', 'email', 'password', 'role_id'
@@ -39,8 +37,6 @@ class User extends Authenticatable implements JWTSubject
 
     /**
      * The attributes that should be hidden for arrays.
-     *
-     * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
@@ -48,8 +44,6 @@ class User extends Authenticatable implements JWTSubject
 
     /**
      * The attributes that should be cast to native types.
-     *
-     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
@@ -60,7 +54,7 @@ class User extends Authenticatable implements JWTSubject
     /**
      * Checks if a user is an administrator
      */
-    public function isAdmin()
+    public function isAdmin(): bool
     {
         return $this->role_id === 1;
     }
@@ -68,31 +62,40 @@ class User extends Authenticatable implements JWTSubject
     /**
      * Checks if a user is a patron user
      */
-    public function isPatron()
+    public function isPatron(): bool
     {
         return $this->role_id === 2;
     }
 
+    /**
+     * Returns the users JWT Identifier
+     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
     
+    /**
+     * Custom JWT claims
+     */
     public function getJWTCustomClaims()
     {
         return [];
     }
 
     /**
-     * User relationships
+     * Gets all a users blog posts
      */
-
-    public function blogPosts() {
+    public function blogPosts(): HasMany
+    {
         return $this->hasMany(BlogPost::class);
     }
 
-    public function comments()
+    /**
+     * Gets all a users comments
+     */
+    public function comments(): HasMany
     {
-        return $this->morphMany(Comment::class, 'commentable');
+        return $this->hasMany(Comment::class);
     }
 }
